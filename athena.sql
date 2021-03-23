8075a07104247c5e0b399e90adecaa2f0c207c23
Anwar Sumawang <asumawang@gmail.com>
2021-03-23 14:01:03 +0800

    Modify athena.sql to query different counts and totals Modify athena_results.csv to reflect results of the new query

--- athena.sql
+++ athena.sql
@@ -1,3 +1,23 @@
-SELECT COUNT(museum_type) as museum_type_count, museum_type, city_administrative_location, state_administrative_location
-FROM museums_extracted
-GROUP BY museum_type, city_administrative_location, state_administrative_location
+WITH museums_count_by_type_per_city AS (SELECT COUNT(museum_type) AS "Count", museum_type AS "Type", city_administrative_location AS "City", state_administrative_location AS "State"
+FROM "museums"."museums_extracted"
+GROUP BY museum_type, city_administrative_location, state_administrative_location),
+
+museums_total_cities AS (SELECT SUM("Count") AS "Count", 'All' AS "Type", "City", "State" 
+FROM museums_count_by_type_per_city
+GROUP BY "City", "State"),
+
+museums_total_by_type_per_state AS (SELECT SUM("Count") AS "Count", "Type", 'All' AS "City", "State"
+FROM museums_count_by_type_per_city
+GROUP BY "Type", "State"),
+
+museums_type_count_by_state AS (SELECT COUNT("Type") AS "Count", 'ALL' AS "Type", 'All' AS "City", "State" 
+FROM museums_count_by_type_per_city
+GROUP BY "State")
+
+SELECT "Count", "Type", "City", "State" FROM museums_count_by_type_per_city
+UNION
+SELECT "Count", "Type", "City", "State" FROM museums_total_cities
+UNION
+SELECT "Count", "Type", "City", "State" FROM museums_total_by_type_per_state
+UNION
+SELECT "Count", "Type", "City", "State" FROM museums_count_by_state
